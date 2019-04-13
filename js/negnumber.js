e002a3584a6963aa811712a23d989788d5f49a5d
Janani689 <janani689@gmail.com>
2019-04-13 18:25:51 -0400

    grammer fix

--- js/negnumber.js
+++ js/negnumber.js
@@ -3,10 +3,10 @@ function negNumber(x,y,z){
 var product = x * y * z;
 
 if (product< 0) {
-	product= alert("the product is a negative number");
+	alert("the product is a negative number");
 } 
 else {
-	product= alert("the product is a positive number");
+	 alert("the product is a positive number");
 	}
 
 }
