152631248454b84ce7687b6713906469e443305c
Janani689 <janani689@gmail.com>
2019-04-13 18:17:15 -0400

    Update negnumber.js

--- js/negnumber.js
+++ js/negnumber.js
@@ -1,13 +1,12 @@
 function negNumber(x,y,z){
 
-var finalNumber = x * y * z;
+var product = x * y * z;
 
-if (finalNumber < 0) {
-	alert("the product is a negative number.")
+if (product< 0) {
+	product= alert("the product is a negative number");
 } 
 else {
-	alert("the product is a positive number.")
+	product= alert("the product is a positive number");
 	}
-}
 
-negNumber(2,4,6);
+}
