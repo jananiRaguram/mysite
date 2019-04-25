7113d34d5dbae05ca3609009b0e52baa0dc6accb
Janani689 <47528135+Janani689@users.noreply.github.com>
2019-04-25 09:15:00 -0400

    Update orderednumbers.js

--- js/orderednumbers.js
+++ js/orderednumbers.js
@@ -1,7 +1,7 @@
 function numberOrder() {
 	var x = prompt ("enter a number");	
 	var y = prompt ("enter another number");
-	var z = prompt ("enter the last number");
+	var z = prompt ("enter the final number");
 
 	var numbers = [ x, y, z];
 	numbers.sort (function (a,b){
