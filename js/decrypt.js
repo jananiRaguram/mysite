529545ae6f8b06f5d6f6da6c2f07fea23e95a4bb
Janani689 <janani689@gmail.com>
2019-05-26 18:03:35 -0400

    cipher

---
+++ js/decrypt.js
@@ -0,0 +1,16 @@
+function decrypt(){
+  let userInput = prompt("type your message here");
+  let str = "";
+  let sentance= userInput.split(" ");
+  for(let i= 0 ; i<sentance.length; i++){
+      let otherVowels = sentance[i].replace(/e/g,"a").replace(/o/g,"i").replace(/y/g,"u").replace(/E/g,"A").replace(/O/g,"I").replace(/Y/g,"U");
+      if(otherVowels==sentance[i]){
+      let replaceVowels = otherVowels.replace(/a/g,"e").replace(/i/g,"o").replace(/u/g,"y").replace(/I/g,"O").replace(/A/g,"E").replace(/U/g,"Y");
+        str += replaceVowels + " ";
+        document.getElementById("message").innerHTML=(str.trim());
+       }else{
+         str += otherVowels + " ";
+        document.getElementById("message").innerHTML= (str.trim());
+       }
+  }
+}
