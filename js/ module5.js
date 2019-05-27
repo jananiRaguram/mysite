e998a9dd870be3f7d8f8358ba364161d80c86134
Janani689 <janani689@gmail.com>
2019-05-27 08:35:28 -0400

    Update  module5.js

--- js/ module5.js
+++ js/ module5.js
@@ -3,15 +3,15 @@ function secret(){
   let str = "";
   let sentance= userInput.split(" ");
   for(let i= 0 ; i<sentance.length; i++){
-      let replaceVowels = sentance[i].replace(/a/g,"e").replace(/y/g,"a").replace(/i/g,"o").replace(/u/g,"y").replace(/A/g,"E").replace(/O/g,"U").replace(/U/g,"Y");
-      if(replaceVowels!==sentance[i]){
-        let otherVowels = replaceVowels.replace(/E/g,"I").replace(/O/g,"I").replace(/Y/g,"U");
+      let replaceVowels = sentance[i].replace(/a/g,"e").replace(/i/g,"o").replace(/u/g,"y").replace(/I/g,"O").replace(/A/g,"E").replace(/U/g,"Y");
+      if(replaceVowels==sentance[i]){
+        let otherVowels = replaceVowels.replace(/e/g,"a").replace(/o/g,"i").replace(/y/g,"u").replace(/E/g,"A").replace(/O/g,"I").replace(/Y/g,"U");
         str += otherVowels + " ";
-        document.getElementById("ciphertext").innerHTML= (str.trim());
+        document.getElementById("ciphertext").innerHTML=(str.trim());
        }else{
-         str += replaceVowels.replace(/o/g,"u").replace(/e/g,"o")  + " ";
+         str += replaceVowels + " ";
         document.getElementById("ciphertext").innerHTML= (str.trim());
 
-    }
+       }
   }
 }
